ec9e692c2219a2822c84514a6dd3a58148bbc246
asakasamo <a.sakasamo@gmail.com>
2019-03-03 18:05:33 -0500

    Refactor: Turn all of the inline HTML into dynamic Javascript - Move the data related to each drum into its own file - Restructure logical flow: first import drum data, then add listeners

---
+++ 1 - Javascript Drum Kit/DrumData.js
@@ -0,0 +1,58 @@
+const DrumData = [
+   {
+      keyCode: 65,
+      key: "A",
+      label: "clap",
+      src: "sounds/clap.wav"
+   },
+   {
+      keyCode: 83,
+      key: "S",
+      label: "hihat",
+      src: "sounds/hihat.wav"
+   },
+   {
+      keyCode: 68,
+      key: "D",
+      label: "kick",
+      src: "sounds/kick.wav"
+   },
+   {
+      keyCode: 70,
+      key: "F",
+      label: "openhat",
+      src: "sounds/openhat.wav"
+   },
+   {
+      keyCode: 71,
+      key: "G",
+      label: "boom",
+      src: "sounds/boom.wav"
+   },
+   {
+      keyCode: 72,
+      key: "H",
+      label: "ride",
+      src: "sounds/ride.wav"
+   },
+   {
+      keyCode: 74,
+      key: "J",
+      label: "snare",
+      src: "sounds/snare.wav"
+   },
+   {
+      keyCode: 75,
+      key: "K",
+      label: "tom",
+      src: "sounds/tom.wav"
+   },
+   {
+      keyCode: 76,
+      key: "L",
+      label: "tink",
+      src: "sounds/tink.wav"
+   }
+];
+
+export default DrumData;
