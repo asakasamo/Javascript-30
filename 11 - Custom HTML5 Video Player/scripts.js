ecc73faa21e9d444f4e3a6a9e12d3e0ec26ee2e0
asakasamo <a.sakasamo@gmail.com>
2019-03-16 15:49:25 -0400

    Add ranges' functionality (volume and play speed)

--- 11 - Custom HTML5 Video Player/scripts.js
+++ 11 - Custom HTML5 Video Player/scripts.js
@@ -25,6 +25,10 @@ function skip() {
    video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+   video[this.name] = this.value;
+}
+
 /** Hook up the event listeners */
 
 video.addEventListener("click", togglePlay);
@@ -36,3 +40,8 @@ toggle.addEventListener("click", togglePlay);
 skipButtons.forEach((button) => {
    button.addEventListener("click", skip);
 });
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+   range.addEventListener("mousemove", handleRangeUpdate)
+);
