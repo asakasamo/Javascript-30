1adde01fe69b6a414df764fb9df9b00fce40a755
asakasamo <a.sakasamo@gmail.com>
2019-03-16 15:34:45 -0400

    Add play toggle functionality

--- 11 - Custom HTML5 Video Player/scripts.js
+++ 11 - Custom HTML5 Video Player/scripts.js
@@ -1,5 +1,29 @@
 /** Get our elements **/
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 /** Build our functions **/
 
+function togglePlay() {
+   if (video.paused) {
+      video.play();
+   } else {
+      video.pause();
+   }
+}
+
+function updateButton() {
+   console.log("update the button");
+   toggle.textContent = video.paused ? "►" : "❚ ❚";
+}
+
 /** Hook up the event listeners */
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
+toggle.addEventListener("click", togglePlay);
